4851685d5984fda71a029a096d428a2da9d3ddf8
shehata18 <msb330033@gmail.com>
2026-02-14 16:36:17 +0200

    modify

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello from Flaskkkkk 🚀</h1><p>Deployed on Ubuntu VM</p>"
+    return "<h1>Hello from Flaskkkkkyyyyyyyyyyy 🚀</h1><p>Deployed on Ubuntu VM</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
